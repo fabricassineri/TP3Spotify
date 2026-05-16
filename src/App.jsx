2622690f80a2155328c7526ecf56159b7c6acf7c
mateognovaresio <mnovaresio@facultad.sanfrancisco.utn.edu.ar>
2026-05-16 18:45:06 -0300

    buscador de artistas y seccion favoritos en home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { FavoritesProvider } from './context/FavoritesContext'
 import HomePage from './pages/HomePage'
 import ArtistPage from './pages/ArtistPage'
 import AlbumPage from './pages/AlbumPage'
@@ -7,24 +8,26 @@ import './App.css'
 function App() {
   return (
     <BrowserRouter>
-      <div className="app">
-        <header className="app-header">
-          <Link to="/" className="logo">SpotiTP</Link>
-        </header>
+      <FavoritesProvider>
+        <div className="app">
+          <header className="app-header">
+            <Link to="/" className="logo">SpotiTP</Link>
+          </header>
 
-        <main className="app-main">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/artist/:id" element={<ArtistPage />} />
-            <Route path="/album/:id" element={<AlbumPage />} />
-            <Route path="*" element={<h1>404 - Pagina no encontrada</h1>} />
-          </Routes>
-        </main>
+          <main className="app-main">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/artist/:id" element={<ArtistPage />} />
+              <Route path="/album/:id" element={<AlbumPage />} />
+              <Route path="*" element={<h1>404 - Pagina no encontrada</h1>} />
+            </Routes>
+          </main>
 
-        <footer className="app-footer">
-          <p>TP3 - Desarrollo de Software UTN</p>
-        </footer>
-      </div>
+          <footer className="app-footer">
+            <p>TP3 - Desarrollo de Software UTN</p>
+          </footer>
+        </div>
+      </FavoritesProvider>
     </BrowserRouter>
   )
 }
